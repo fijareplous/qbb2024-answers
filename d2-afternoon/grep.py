98aa7d0f015e9cefff45b2032abe168ab940ceba
Fijare Plous <fplous1@jhu.edu>
2024-09-05 11:25:20 -0400

    Updated grep.py to have correct answers based on Day 3 morning group work

--- d2-afternoon/grep.py
+++ d2-afternoon/grep.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
+#Grep is like a search function; prints lines in a file where it matches
 
 import sys
 
-file = open(sys.argv[1])
+file = open(sys.argv[2])
+#Allows us to type in any file into terminal, sys.argv is an attribute/list, the list contains 
+#instructions, [./grep.py, '<word being searched>', '<file name>'] 
+
+pattern = sys.argv[1]
+#Specifies which word/phrase we're trying to search for
 
 for line in file:
     line = line.rstrip("\n")
-    print(line)
+    #If pattern exists in the line:
+    if pattern in line: 
+        print(line)
 
 file.close()
-
 
 
 
